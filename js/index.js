cab5aa970967dfdfbd9a941e2788fd2a4c13f814
alesiods <alesioadrover@gmail.com>
2022-06-14 15:04:20 -0300

    pulido

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 document.getElementById("verMas").addEventListener("click", () => {
     document.getElementById("verMas").style.display = "none";
+    document.getElementById("verMenos").style.display = "block";
     document.querySelectorAll(".ocultar").forEach((element) => {
         element.classList.add("mostrar");
+    });
+});
+
+
+document.getElementById("verMenos").addEventListener("click", () => {
+    document.getElementById("verMenos").style.display = "none";
+    document.getElementById("verMas").style.display = "block";
+    document.querySelectorAll(".mostrar").forEach((element) => {
+        element.classList.remove("mostrar");
     });
 });
